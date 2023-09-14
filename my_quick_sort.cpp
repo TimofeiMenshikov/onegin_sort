14a8f06014cda3a690c246bb240687a8f7a6a477
Timofei Menshikov <tim777men@gmail.com>
2023-09-14 17:09:11 +0300

    Update my_quick_sort.cpp

--- my_quick_sort.cpp
+++ my_quick_sort.cpp
@@ -9,6 +9,15 @@ void swap_numbers(int* num_1, int* num_2)
 	*num_2 = temp;
 }
 
+
+void arr_int_ncpy(int* const dest, const int* const src, size_t n)
+{
+	for (size_t int_number = 0; int_number < n; int_number++)
+	{
+		dest[int_number] = src[int_number];
+	}
+}
+
 void print_state(const int* const  arr, const size_t size, const size_t left, const size_t right, const size_t sep_postition)
 {
 	for (size_t number = 0; number < size; number++)
@@ -56,10 +65,15 @@ void print_state(const int* const  arr, const size_t size, const size_t left, co
 }
 
 
-void my_quick_sort(int*  arr, const size_t size)
+int* my_quick_sort(int*  arr, const size_t size)
 {
-	printf("size %zu \n", size);
+	if (size <= 1)
+	{
+		return arr;
+	}
 
+	//printf("size %zu \n", size);
+	putchar('\n');
 
 	size_t left = 0;
 	size_t right = size - 1;
@@ -73,6 +87,8 @@ void my_quick_sort(int*  arr, const size_t size)
 	while (left < right)
 	{
 		print_state(arr, size, left, right, sep_postition);
+
+		printf("sep_postition: %zu\n", sep_postition);
 
 		size_t is_jump_left = 0;
 
@@ -106,11 +122,14 @@ void my_quick_sort(int*  arr, const size_t size)
 			if (sep_postition == left_sort_pos)
 			{
 				sep_postition = right_sort_pos;
-			}
 
-			if (sep_postition == right_sort_pos)
+				//printf("sep_postition is changed \n");
+			}
+			else if (sep_postition == right_sort_pos)
 			{
 				sep_postition = left_sort_pos;
+
+				//printf("sep_postition is changed \n");			
 			}
 
 			right_sort_pos = -1;
@@ -121,31 +140,27 @@ void my_quick_sort(int*  arr, const size_t size)
 
 		}
 
-
-
-
 		left += is_jump_left;
 		right -= is_jump_right;
 	
-	print_state(arr, size, left, right, sep_postition);
-
 	}
 
-	if (size <= 1)
-	{
-		return;
-	}
+	char* src = my_quick_sort(arr, sep_postition);
 
-	//my_quick_sort(arr, sep_postition);
+	arr_int_ncpy(arr, src, sep_postition);
 
-	//my_quick_sort(arr + sep_postition + 1, 2);
+	src = my_quick_sort(arr + sep_postition + 1, size - sep_postition - 1);
+
+	arr_int_ncpy(arr + sep_postition + 1, src, size - sep_postition - 1);
+
+	return arr;
 }
 
 
 int main()
 {
-	//int arr[] = {1, 8, 3, 5, 6, 7};
-	//const size_t size = 6;
+	int arr[] = {1, 8, 3, 5, 6, 7};
+	const size_t size = 6;
 
 	//int arr[] = {5, 2, 15, 3, 1, 16, 10, 5, 10, 13, 4, 12, 6, 6, 22, 20};
 	//const size_t size = 16;
@@ -153,8 +168,8 @@ int main()
 	//int arr[] = {5, 2, 15, 3, 1, 16, 10, 5, 10, 3, 4, 12, 6, 6, 22, 20};
 	//const size_t size = 16;
 
-	int arr[] = {9, 8, 7, 1, 5, 4, 3};
-	const size_t size = 7;
+	//int arr[] = {9, 8, 7, 1, 5, 4, 3};
+	//const size_t size = 7;
 
 	my_quick_sort(arr, size);
 }
